96507ff9e14db7cfe49cb3dbb0509bf0bd501e3d
HaDuve <48185176+HaDuve@users.noreply.github.com>
2020-02-28 16:39:13 +0100

    Tutorial Stuff + Creating own level
    unity terrain fucks the 3rd person controller up

---
+++ NavMesh Example Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera Cam;
+    public NavMeshAgent Agent;
+    public ThirdPersonCharacter Character;
+
+    void Start()
+    {
+        Agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Move Agent
+                Agent.SetDestination(hit.point);
+            }
+        }
+
+        if (Agent.remainingDistance > Agent.stoppingDistance)
+        {
+            Character.Move(Agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            Character.Move(Vector3.zero, false, false);
+        }
+        
+
+    }
+}
